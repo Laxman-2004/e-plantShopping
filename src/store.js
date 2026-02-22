5bc3cfc1727e6a990044a83b518e8b266c14b8bc
Laxman Sharma <laxmansharma0871@gmail.com>
2026-02-22 12:09:34 -0500

    changes

--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+dispatch(addItem(product));
+dispatch(removeItem(item.name));
  const store = configureStore({
+
     reducer: {
         cart: cartReducer,
     },
 });
+
+const calculateTotalQuantity = () => {
+ return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+  };
+
 export default store
